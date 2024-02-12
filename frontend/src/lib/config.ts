ab3ca613ca1161fd3e9ca405e656c4874726709d
lucasquin <lucaslquintino@gmail.com>
2024-02-12 00:54:10 -0300

    feat: started project with svelte app

---
+++ frontend/src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from "$app/environment"
+
+export const title = "lucasquin website"
+export const description = "my personal website for on my own"
+export const url = dev ? "http://localhost:5173/" : "prod"
