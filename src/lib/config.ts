3fcbaf842ad9dae01751989c19d710f098c588b5
lucasquin <lucaslquintino@gmail.com>
2024-02-18 23:07:29 -0300

    break: reset of project

--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from "$app/environment"
-
-export const title = "lucasquin website"
-export const description = "my personal website for on my own"
-export const url = dev ? "http://localhost:5173/" : "prod"
